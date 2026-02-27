56f61d57c9fbb1bc7223fc37c4fea0639ed59500
vidushi300421 <viduship3004@gmail.com>
2026-02-27 22:22:53 +0530

    fix render port binding to 0.0.0.0

--- server/index.ts
+++ server/index.ts
@@ -83,7 +83,7 @@ app.use((req, res, next) => {
   }
 
   const port = parseInt(process.env.PORT || "3000", 10);
-  httpServer.listen(port, "127.0.0.1", () => {
+  httpServer.listen(port, "0.0.0.0", () => {
     log(`serving on port ${port}`);
   });
 })();
